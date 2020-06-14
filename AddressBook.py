e910f825870baff9a4880d54d0c571ed6fbe6a4d
max-Z7 <zaraz121801@gmail.com>
2020-06-14 18:49:32 +0300

    0.9.1

--- AddressBook.py
+++ AddressBook.py
@@ -1,6 +1,7 @@
-import re
 #основные переменные{
-v="0.1.1"
+v="0.9.1"
+#file=open('ab', r, w)
+ls={'Моя почта': 'zaraz121801@gmail.com', 'VOID': '00000@email.org'}
 logo=\
 '''		  ┌┐      ┌┐
 		  ├┤      ├<
@@ -9,41 +10,50 @@ logo=\
 help='''
 help		выводит это сообщение
 exit		выход
-list <страница>	список контактных данных
+list		список контактных данных
 type		создание/редактирование контакта
 find <имя>	поиск контакта
 del  <имя>	удалить контакт
 info		о программе
 '''
-e=' Недоступная команда.\n'
-h='(для помощи, введите \"help\").\n'
+
 #} функции{
-def E(x):
-	print(e+x*h)
-
-
-#} сама программа{
+def E():
+	print('Недоступная команда.\n(для помощи, введите \"help\").\n')
+#} основной алгоритм{
 print('\n\n'+logo+'\n\n')
 while True:
 	try:
-		velue=input('> ')
-		o=re.findall(r'\w+', velue)[0]
-		if o=='exit':
+		command=input('> ')
+		w1=command[:4]
+		if w1=='exit':
 			break
-		elif o=='help':
+		elif w1=='help':
 			print(help)
-		elif o=='type':
-			E(0)
-		elif o=='list':
-			E(0)
-		elif o=='find':
-			E(0)
-		elif o=='del':
-			E(0)
-		elif o=='info':
+		elif w1=='type':
+			name=input('Имя контакта: ')
+			address=input('Почта или номер телефона: ')
+			ls[name]=address
+		elif w1=='list':
+			for n, a in ls.items():
+				print('	{}: {}'.format(n, a))
+		elif w1=='info':
 			print('{}\nAddressBook\nV {}\n(с) 2020 Terlyk Maksim'.format(logo, v))
+		elif w1=='find':
+			w2=command[5:]
+			if w2 in ls:
+				print('	{}: {}'.format(w2, ls[w2]))
+			else:
+				print('Контакт "{}" не найден\n'.format(w2))
+		elif w1=='del ':
+			w2=command[4:]
+			
+			if w2 in ls:
+				del ls[w2]
+			else:
+				print('Контакт "{}" не найден\n'.format(w2))
 		else:
-			E(1)
+			E()
 	except:
-		E(1)
+		E()
 #}.
