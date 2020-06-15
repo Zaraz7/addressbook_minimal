af2ab72127117196a88c2d9fe0f2851cc1c3c6d0
max-Z7 <zaraz121801@gmail.com>
2020-06-15 10:12:03 +0300

    0.9.5

--- AddressBook.py
+++ AddressBook.py
@@ -1,7 +1,14 @@
-#основные переменные{
-v="0.9.1"
-#file=open('ab', r, w)
-ls={'Моя почта': 'zaraz121801@gmail.com', 'VOID': '00000@email.org'}
+import os
+import pickle
+#
+v="0.9.5"
+path='ab.data'
+if os.access(path, os.F_OK) and os.path.getsize(path) > 0:
+	file=open(path, 'rb')
+	ls=pickle.load(file)
+	file.close()
+else:
+	ls={}
 logo=\
 '''		  ┌┐      ┌┐
 		  ├┤      ├<
@@ -14,19 +21,24 @@ list		список контактных данных
 type		создание/редактирование контакта
 find <имя>	поиск контакта
 del  <имя>	удалить контакт
+save		сохранить изменения
 info		о программе
 '''
-
-#} функции{
+#
 def E():
 	print('Недоступная команда.\n(для помощи, введите \"help\").\n')
-#} основной алгоритм{
+def save():
+	file=open(path, 'wb')
+	pickle.dump(ls, file)
+	file.close()
+#
 print('\n\n'+logo+'\n\n')
 while True:
 	try:
 		command=input('> ')
 		w1=command[:4]
 		if w1=='exit':
+			if 
 			break
 		elif w1=='help':
 			print(help)
@@ -35,8 +47,11 @@ while True:
 			address=input('Почта или номер телефона: ')
 			ls[name]=address
 		elif w1=='list':
-			for n, a in ls.items():
-				print('	{}: {}'.format(n, a))
+			if len(ls)>0:
+				for n, a in ls.items():
+					print('	{}: {}'.format(n, a))
+			else:
+				print('Книга пока пуста')
 		elif w1=='info':
 			print('{}\nAddressBook\nV {}\n(с) 2020 Terlyk Maksim'.format(logo, v))
 		elif w1=='find':
@@ -47,13 +62,15 @@ while True:
 				print('Контакт "{}" не найден\n'.format(w2))
 		elif w1=='del ':
 			w2=command[4:]
-			
 			if w2 in ls:
 				del ls[w2]
 			else:
 				print('Контакт "{}" не найден\n'.format(w2))
+		elif w1=='save':
+			save()
 		else:
 			E()
+#?		print(ls)
 	except:
 		E()
-#}.
+
